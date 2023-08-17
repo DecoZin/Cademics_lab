22ec1bc07d2e54e868340464829c443932ce1331
André Lamego Reis de Castro <andrelamegobh@hotmail.com>
2023-08-16 22:35:57 -0300

    Module 03

---
+++ Module 03 - multiplexor/multiplexor.v
@@ -0,0 +1,14 @@
+module multiplexor 
+#(
+  parameter integer WIDTH = 5
+  )
+  (
+    input wire sel,
+    input wire [WIDTH-1:0] in0, in1,
+    output reg [WIDTH-1:0] mux_out
+  );
+
+  always @(sel, in0, in1)
+    mux_out = sel ? in1 : in0;
+  
+endmodule
