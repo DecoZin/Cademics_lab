0726a76f9d6c642e6e6cf1ce7d458fa3b937cd7c
André Lamego Reis de Castro <andrelamegobh@hotmail.com>
2023-08-21 21:14:11 -0300

    Module 09

---
+++ Module 09 - cntr/counter.v
@@ -0,0 +1,26 @@
+module counter 
+#(
+  parameter WIDTH = 5
+) (
+  input wire clk  ,
+  input wire rst  ,
+  input wire load ,
+  input wire enab ,
+  input wire [WIDTH-1:0] cnt_in,
+  output reg [WIDTH-1:0] cnt_out
+);
+  
+  always @(posedge clk)
+    begin 
+      if (rst)
+        cnt_out <= 'b0;
+      else
+        begin 
+          if (load) 
+            cnt_out <= cnt_in;
+          else if (enab)
+            cnt_out <= cnt_out + 1;
+        end
+    end
+
+endmodule
