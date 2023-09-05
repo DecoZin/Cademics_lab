ad9ad9897de85a9a70935174c697ab65b3bcf16d
André Lamego Reis de Castro <andrelamegobh@hotmail.com>
2023-09-05 11:21:12 -0300

    Module 13

--- Module  13 - lib/lib.v
+++ Module  13 - lib/lib.v
@@ -25,6 +25,17 @@ module priority7 ( y, a ) ;
   output reg  [2:0] y ;
   input  wire [7:1] a ;
 
+  always @*
+    begin
+      y = 0;
+      if (a[1] == 1) y = 1; else
+      if (a[2] == 1) y = 2; else
+      if (a[3] == 1) y = 3; else
+      if (a[4] == 1) y = 4; else
+      if (a[5] == 1) y = 5; else
+      if (a[6] == 1) y = 6; else
+      if (a[7] == 1) y = 7;
+    end
 
 endmodule
 `endif
@@ -42,10 +53,20 @@ module latchrs ( q, e, d, r, s ) ;
   output reg  q ;
   input  wire e, d, r, s ;
 
-// do not remove comment below
-// cadence async_set_reset "s, r"
+  // do not remove comment below
+  // cadence async_set_reset "s, r"
 
-
+  always @*
+    begin
+    if (!s)      
+       q <= 1;
+      else  
+        if (!r)
+          q <= 0;
+        else
+          if (e)
+            q <= d;
+    end
 
 endmodule
 `endif
@@ -62,6 +83,17 @@ module dffrs ( q, c, d, e, r, s ) ;
   output reg  q ;
   input  wire c, d, e, r, s ;
 
+  always @(posedge c or s or r)
+      begin
+      if (!s)      
+         q <= 1;
+        else  
+          if (!r)
+            q <= 0;
+          else
+            if (e)
+              q <= d;
+      end
 
 endmodule
 `endif
@@ -79,6 +111,13 @@ module drive8 ( y, a, e ) ;
   input  wire [7:0] a ;
   input  wire       e ;
 
+  always @*
+    begin
+      if (!e)
+        y = 'bz;
+      else
+        y = a;
+    end
 
 endmodule
 `endif
